9f41758081ea384a5c1383939eb0a0ded2a756dd
timmyha619 <timmyha619@yahoo.com>
2021-08-30 15:40:54 +0700

    add user.js in user folder, fix userRoute.js

--- myapp/backend/user/userRoute.js
+++ myapp/backend/user/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.model");
+const User = require("./user");
 
 //Get all users information
 router.get("/", async (req, res) => {
