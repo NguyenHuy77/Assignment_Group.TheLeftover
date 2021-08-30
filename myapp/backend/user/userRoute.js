6ce3ccd3ba3cf3fa93cf85af149f19876882b0cc
timmyha619 <timmyha619@yahoo.com>
2021-08-30 15:11:54 +0700

    fix require of userRoute.js

--- myapp/backend/user/userRoute.js
+++ myapp/backend/user/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./user");
+const User = require("../models/user.model");
 
 //Get all users information
 router.get("/", async (req, res) => {
